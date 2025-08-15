e9f981feb9f17659d812a41ec81af2f5df3be3e1
Ti Leggett <tileggett@nvidia.com>
2025-08-15 16:54:50 -0500

    Lots of changes to accommodate new vim plugin, theme changes, and others

--- setup.sh
+++ setup.sh
@@ -1,43 +1,80 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ ! -e ${HOME}/.local ]; then
-    if [ `uname -s` == "Darwin" ]; then
-        ln -sf ${PYLOCAL} `python3 -c "import site; print(site.USER_BASE)"`
-    else
-        install -d ${HOME}/.local/tmp
+# Install Homebrew (macOS)
+if [ `uname -s` == "Darwin" ]; then
+    if $( ! `which -s brew` ); then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
 fi
 
+# Install Terminal Defaults (macOS)
+if [ `uname -s` == "Darwin" ]; then
+    TERM_DEFAULTS=`defaults write com.apple.Terminal "Default Window Settings"`
+    if [ "${TERM_DEFAULTS}" != "Github Dark" ]; then
+        open "Github Dark.terminal"
+        defaults write com.apple.Terminal "Default Window Settings" -string "Github Dark"
+        defaults write com.apple.Terminal "Startup Window Settings" -string "Github Dark"
+    fi
+fi
+
+if ((BASH_VERSINFO < 4 )); then
+    echo "Current running bash version is not greater than 4."
+    if [ `uname -s` == "Darwin" ]; then
+        echo "Installing newer bash via Homebrew."
+        echo "After installation, edit Terminal settings to use newly isntalled bash."
+        brew install bash
+    fi
+    echo "Exiting."
+    exit 1
+fi
+
+PYUSERBASE=`python3 -c "import site; print(site.USER_BASE)"`
+PYLOCAL=${HOME}/.local
+if [ ! -e ${PYLOCAL} ]; then
+    if [ `uname -s` == "Darwin" ]; then
+        [ ! -d ${PYUSERBASE} ] && install -d ${PYUSERBASE}
+        ln -sf ${PYUSERBASE} ${PYLOCAL}
+    else
+        install -d ${PYLOCAL}
+    fi
+fi
+install -d ${PYLOCAL}/tmp
+
 # Install Pygments
-python3 -m pip install --user Pygments
+if $( ! `which -s pygmentize` ); then
+    if [ `uname -s` == "Darwin" ]; then
+        brew install pygments
+    else
+        python3 -m pip install --user Pygments
+    fi
+fi
 
 # Install Pandoc
-PANDOC_VER="3.1.8"
-pushd ${HOME}/.local/tmp
-if [ `uname -s` == "Darwin" -a `uname -m` == "arm64" ]; then
-    [ -f ${HOME}/.local/tmp/pandoc-${PANDOC_VER}-arm64-macOS.zip ] && rm -f ${HOME}/.local/tmp/pandoc-${PANDOC_VER}-arm64-macOS.zip
-    curl -s -S -L -O https://github.com/jgm/pandoc/releases/download/${PANDOC_VER}/pandoc-${PANDOC_VER}-arm64-macOS.zip
-    unzip -q pandoc-${PANDOC_VER}-arm64-macOS.zip -d ${HOME}/.local
-    rm -f pandoc-${PANDOC_VER}-arm64-macOS.zip
-else
-    [ -f ${HOME}/.local/tmp/pandoc-${PANDOC_VER}-linux-amd64.tar.gz ] && rm -f ${HOME}/.local/tmp/pandoc-${PANDOC_VER}-linux-amd64.tar.gz
-    curl -s -S -L -O https://github.com/jgm/pandoc/releases/download/${PANDOC_VER}/pandoc-${PANDOC_VER}-linux-amd64.tar.gz
-    tar zxf pandoc-${PANDOC_VER}-linux-amd64.tar.gz -C ${HOME}/.local
-    rm -f pandoc-${PANDOC_VER}-linux-amd64.tar.gz
-fi
-popd
-
-if [ -L ${HOME}/.local/pandoc ]; then
-    unlink ${HOME}/.local/pandoc
-    ln -sf ${HOME}/.local/pandoc-${PANDOC_VER} ${HOME}/.local/pandoc
-elif [ -d ${HOME}/.local/pandoc ]; then
-    echo "!!!!! ${HOME}/.local/pandoc exists. Moving out of the way. !!!!!"
-    mv ${HOME}/.local/pandoc ${HOME}/.local/pandoc.old
-    ln -sf ${HOME}/.local/pandoc-${PANDOC_VER} ${HOME}/.local/pandoc
-elif [ -e ${HOME}/.local/pandoc ]; then
-    echo "!!!!! ${HOME}/.local/pandoc exists. Unsure what to do. !!!!!"
-elif [ ! -e ${HOME}/.local/pandoc ]; then
-    ln -sf ${HOME}/.local/pandoc-${PANDOC_VER} ${HOME}/.local/pandoc
+if $( ! `which -s pygmentize` ); then
+    if [ `uname -s` == "Darwin" ]; then
+        brew install groff pandoc
+    else
+        PANDOC_VER="3.1.8"
+        pushd ${PYLOCAL}/tmp
+        [ -f ${PYLOCAL}/tmp/pandoc-${PANDOC_VER}-linux-amd64.tar.gz ] && rm -f ${PYLOCAL}/tmp/pandoc-${PANDOC_VER}-linux-amd64.tar.gz
+        curl -s -S -L -O https://github.com/jgm/pandoc/releases/download/${PANDOC_VER}/pandoc-${PANDOC_VER}-linux-amd64.tar.gz
+        tar zxf pandoc-${PANDOC_VER}-linux-amd64.tar.gz -C ${PYLOCAL}
+        rm -f pandoc-${PANDOC_VER}-linux-amd64.tar.gz
+        popd
+        
+        if [ -L ${PYLOCAL}/pandoc ]; then
+            unlink ${PYLOCAL}/pandoc
+            ln -sf ${PYLOCAL}/pandoc-${PANDOC_VER} ${PYLOCAL}/pandoc
+        elif [ -d ${PYLOCAL}/pandoc ]; then
+            echo "!!!!! ${PYLOCAL}/pandoc exists. Moving out of the way. !!!!!"
+            mv ${PYLOCAL}/pandoc ${PYLOCAL}/pandoc.old
+            ln -sf ${PYLOCAL}/pandoc-${PANDOC_VER} ${PYLOCAL}/pandoc
+        elif [ -e ${PYLOCAL}/pandoc ]; then
+            echo "!!!!! ${PYLOCAL}/pandoc exists. Unsure what to do. !!!!!"
+        elif [ ! -e ${PYLOCAL}/pandoc ]; then
+            ln -sf ${PYLOCAL}/pandoc-${PANDOC_VER} ${PYLOCAL}/pandoc
+        fi
+    fi
 fi
 
 # Install Bash-it
@@ -52,16 +89,41 @@ if [ ! -d ${HOME}/.bash_it ]; then
 fi
 
 # Configure Vim
+# Make sure all the necessary directories exist
 for dir in autoload bundle ftdetect ftplugin indent syntax; do
     install -d -o ${USER} ${HOME}/.vim/${dir}
 done
+
+# Download all the Vim bundles
 pushd ${HOME}/.vim/bundle
-for repo in https://github.com/VundleVim/Vundle.vim.git https://github.com/tpope/vim-bundler.git https://github.com/nvie/vim-flake8.git https://github.com/tpope/vim-pathogen.git https://github.com/tpope/vim-projectionist.git https://github.com/Vimjas/vim-python-pep8-indent.git https://github.com/tpope/vim-rails.git https://github.com/tpope/vim-rake.git; do
-    git clone ${repo} 
+declare -A repos
+repos[Vundle.vim]="https://github.com/VundleVim/Vundle.vim.git"
+repos[vim-bundler]="https://github.com/tpope/vim-bundler.git"
+repos[vim-flake8]="https://github.com/nvie/vim-flake8.git"
+repos[vim-pathogen]="https://github.com/tpope/vim-pathogen.git"
+repos[vim-projectionist]="https://github.com/tpope/vim-projectionist.git"
+repos[vim-python-pep8-indent]="https://github.com/Vimjas/vim-python-pep8-indent.git"
+repos[vim-rails]="https://github.com/tpope/vim-rails.git"
+repos[vim-rake]="https://github.com/tpope/vim-rake.git"
+repos[vim-plug.git]="https://github.com/junegunn/vim-plug.git"
+
+for repo in ${!repos[@]}; do
+    if [ -d ${repo} ]; then
+        pushd ${repo}
+        git pull
+        popd
+    else
+        git clone ${repo} 
+    fi
 done
 popd
+
+# If the Plug isn't set to autoload, make it so
+[ ! -e ${HOME}/.vim/autoload/plug.vim ] && ln -sf ${HOME}/.vim/bundler/vim-plug/plug.vim ${HOME}/.vim/autoload/plug.vim
+
 [ -f ${HOME}/.vimrc ] && cp ${HOME}/.vimrc ${HOME}/.vimcrc.bak
 install -o ${USER} vimrc ${HOME}/.vimrc
+vim +PlugInstall +qall
 
 # Instal rc files
 [ -f ${HOME}/.bashrc ] && cp ${HOME}/.bashrc ${HOME}/.bashrc.bak
@@ -70,3 +132,5 @@ install -o ${USER} bashrc ${HOME}/.bashrc
 install -o ${USER} bash_profile ${HOME}/.bash_profile
 [ -f ${HOME}/.lessfilter ] && cp ${HOME}/.lessfilter ${HOME}/.lessfilter.bak
 install -o ${USER} lessfilter ${HOME}/.lessfilter
+
+[ -d ${PYLOCAL}/tmp ] && rm -rf ${PYLOCAL}/tmp
